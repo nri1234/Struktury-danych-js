ff647b2b207c3ca781f3068d72d5337f3c1629af
Norbert Mielczarek <nri1234.nm@gmail.com>
2019-05-21 17:41:45 +0200

    Chane content and coments to project

--- js/script.js
+++ js/script.js
@@ -1,50 +1,80 @@
-'use strict';
-
 var data = [
     {
         id: 'box1',
         title: 'First box',
         content: '<p>Lorem ipsum dolor sit amet!</p>',
-        categories: ['highlighted', 'special-header', 'important']
+        categories: ['highlighted', 'special-header', 'important'],
+        soldOut: true,
+        price: 150,
+        currency: '$',
 	},
     {
         id: 'box2',
         title: 'Second box',
         content: '<p>Lorem ipsum dolor sit amet!</p>',
-        categories: ['special-header', 'important']
+        categories: ['special-header', 'important'],
+        soldOut: true,
+        price: 50,
+        currency: '$',
 	},
     {
         id: 'box3',
         title: 'Third box',
         content: '<p>Lorem ipsum dolor sit amet!</p>',
-        categories: ['highlighted', 'important']
+        categories: ['highlighted', 'important'],
+        soldOut: true,
+        price: 50,
+        currency: '$',
 	},
     {
         id: 'box4',
         title: 'Fourth box',
         content: '<p>Lorem ipsum dolor sit amet!</p>',
-        categories: ['highlighted']
+        categories: ['highlighted'],
+        soldOut: false,
+        price: 70,
+        currency: '$',
 	},
     {
         id: 'box5',
-        title: 'Fifth box',
+        title: 'Fifth',
         content: '<p>Lorem ipsum dolor sit amet!</p>',
-        categories: []
+        categories: [],
+        soldOut: true,
+        price: 35,
+        currency: '$',
 	},
 ];
 
-var output = document.getElementById('boxes');
-
+var container = document.getElementById('boxes');
+//zmieniajac output musze zmienic tez appendChild
 for (var i = 0; i < data.length; i++) {
 
-    var box = document.createElement('categories');
+    var box = document.createElement('div');
+
     box.id = data[i].id;
 
     var header = document.createElement('header');
     header.innerHTML = data[i].title;
     box.appendChild(header);
+
     box.innerHTML += data[i].content;
     box.className = 'box ' + data[i].categories.join(' ');
 
-    output.appendChild(box);
+    if (data[i].soldOut === true) { //jesli dane z tablicy sa true to stworz mi nowy paragraf ktory pokaze napis sold out
+        var h4 = document.createElement('h4');
+        h4.innerHTML = 'SOLD OUT'; // data[i].soldOut;
+        box.appendChild(h4); // to mi go tworzy
+    }
+    if (data[i].soldOut === false) {
+        var h4 = document.createElement('h4');
+        h4.innerHTML = '<br>';
+        box.appendChild(h4);
+    }
+
+
+    box.innerHTML += data[i].price;
+    box.innerHTML += data[i].currency;
+
+    container.appendChild(box);
 }
